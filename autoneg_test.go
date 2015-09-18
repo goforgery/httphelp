c47d941bac9e622177f22064f97f8b74f48816aa
Ric Allinson <capecodehq@yahoo.co.uk>
2015-09-18 15:30:44 -0700

    Updated tests to use simplebd

--- autoneg_test.go
+++ autoneg_test.go
@@ -1,33 +1,40 @@
 package httphelp
 
 import (
+	. "github.com/ricallinson/simplebdd"
 	"testing"
 )
 
-var chrome = "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
+func TestCreate(t *testing.T) {
 
-func TestParseAccept(t *testing.T) {
-	alternatives := []string{"text/html", "image/png"}
-	content_type := Negotiate(chrome, alternatives)
-	if content_type != "image/png" {
-		t.Errorf("got %s expected image/png", content_type)
-	}
+	Describe("Create()", func() {
 
-	alternatives = []string{"text/html", "text/plain", "text/n3"}
-	content_type = Negotiate(chrome, alternatives)
-	if content_type != "text/html" {
-		t.Errorf("got %s expected text/html", content_type)
-	}
+		chrome := "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
 
-	alternatives = []string{"text/n3", "text/plain"}
-	content_type = Negotiate(chrome, alternatives)
-	if content_type != "text/plain" {
-		t.Errorf("got %s expected text/plain", content_type)
-	}
+		It("should return [image/png]", func() {
+			alternatives := []string{"text/html", "image/png"}
+			content_type := Negotiate(chrome, alternatives)
+			AssertEqual(content_type, "image/png")
+		})
 
-	alternatives = []string{"text/n3", "application/rdf+xml"}
-	content_type = Negotiate(chrome, alternatives)
-	if content_type != "text/n3" {
-		t.Errorf("got %s expected text/n3", content_type)
-	}
+		It("should return [text/html]", func() {
+			alternatives := []string{"text/html", "text/plain", "text/n3"}
+			content_type := Negotiate(chrome, alternatives)
+			AssertEqual(content_type, "text/html")
+		})
+
+		It("should return [text/plain]", func() {
+			alternatives := []string{"text/n3", "text/plain"}
+			content_type := Negotiate(chrome, alternatives)
+			AssertEqual(content_type, "text/plain")
+		})
+
+		It("should return [text/n3]", func() {
+			alternatives := []string{"text/n3", "application/rdf+xml"}
+			content_type := Negotiate(chrome, alternatives)
+			AssertEqual(content_type, "text/n3")
+		})
+	})
+
+	Report(t)
 }
